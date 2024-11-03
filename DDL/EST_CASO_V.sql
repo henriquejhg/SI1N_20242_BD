3df8843705a3d920fed60f55f29270147ae9dc56
Luiz Henrique Lenci <henriquelenci@gmail.com>
2024-11-02 21:48:53 -0300

    Update EST_CASO_V.sql

--- DDL/EST_CASO_V.sql
+++ DDL/EST_CASO_V.sql
@@ -1,1 +1,79 @@
+#criando schema
+create schema EC5_SI1N;
 
+#usando banco
+use EC5_SI1N;
+
+#criando tabela cliente
+create table  clientes(
+id_cli int primary key,
+nome_cli varchar(20) not null,
+cpf_cli int not null,
+data_nasc_cli date not null,
+end_cli varchar(30) not null,
+status_fid_cli varchar(20) not null,
+id_prod int not null,
+id_fornec int not null,
+id_vend int not null,
+id_pag int not null,
+constraint fk_id_prod foreign key (id_prod) references produtos(id_prod),
+constraint fk_id_fornec foreign key(id_fornec) references fornecedores(id_fornec),
+constraint fk_id_vend foreign key(id_vendas) references vendas (id_vendas),
+constraint fk_id_pag foreign key (id_pagamentos) references pagamentos(id_pag)
+);
+
+#criando tabela produtos
+create table produtos(
+id_prod int primary key,
+cat_prod varchar(10) not null,
+disp_prod int not null,
+preco_prod varchar(100) not null,
+id_fornec int not null,
+constraint fk_id_fornec foreign key (id_fornec) references fornecedores (id_fornec)
+);
+
+#criando tabela fornecedores
+create table fornecedores(
+id_fornec int primary key,
+nome_fornec varchar(20) not null,
+tel_fornec varchar(12),
+prod_fornec varchar (20),
+id_prod int not null,
+constraint fk_id_prod foreign key (id_prod) references produtos (id_prod)
+);
+
+#criando tabela vendas
+create table vendas(
+id_vend int primary key,
+quant_vend varchar(10) not null,
+id_prod int not null,
+id_fornec int not null,
+id_cli int not null,
+constraint fk_id_prod foreign key (id_prod) references produtos(id_prod),
+constraint fk_id_fornec foreign key (id_fornec) references fornecedores(id_fornec),
+constraint fk_id_fornec foreign key (id_cli) references clientes(id_cli)
+);
+
+#criando tabela pagamentos
+create table pagamentos(
+id_pag int primary key,
+form_pag varchar (10) not null,
+id_prod int not null,
+id_vend int not null,
+id_cli int not null,
+constraint fk_id_prod foreign key (id_prod) references produtos(id_prod),
+constraint fk_id_vend foreign key (id_vend) references vendas(id_vend),
+constraint fk_id_cli foreign key (id_cli) references clientes(id_cli)
+);
+
+
+#Alter das tabelas 
+alter table clientes add email varchar(20);
+alter table produtos drop column cat_prod;
+alter table fornecedores modify column nome_fornec varchar(200);
+alter table vendas change column quant_vend lotes_prod varchar(200);
+alter table pagamentos add tipo_moeda varchar(20);
+
+
+#DROP da tabela 
+drop table fornecedores;
